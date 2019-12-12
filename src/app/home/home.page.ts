1e8ce326148f1da0b042284970e5d63eb89a00a7
Aldimir Aleksandrov <aldimirprincipal@gmail.com>
2019-12-12 01:14:40 +0100

    Entrega AE7

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  option: string = "";
+
   constructor() {}
 
+  save_Option(value){
+    this.option = value;
+  }
+  
 }
