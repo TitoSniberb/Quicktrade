d80a14b8e23b490b16c56675154e347eb368ef2d
2dam <2dam@c20p6.alumnado.flo>
2020-02-06 20:41:17 +0100

    E y entrega examen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { ProductoService } from '../services/producto.service';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +11,33 @@ import { Component } from '@angular/core';
 export class HomePage {
   id: string;
 
-  constructor(){}
+  cantidad: number = 0;
+
+  constructor(private _toast: ToastController, private _productosService: ProductoService){}
 
   ngOnInit(){
     this.id = "mKilGUHPNfex87XCNefC601AUhX2";
+
+    //--E  ---------------------------------------------------- 
+    let ref = this._productosService.getUsuarios();
+
+    ref.once("value", snap => {
+      this.cantidad = snap.val().cantidadFav;
+    })
+    //--E  ---------------------------------------------------- 
   }
+//--E  ---------------------------------------------------- 
+  async toast_HowManyFavs() {
+    const toast = await this._toast.create({
+      message: 'Actualmente tienes ' + this.cantidad + ' productos en favoritos.',
+      duration: 2000,
+      position: 'bottom',
+    });
+    toast.present();
+  }
+
+  favsQuantity(){
+    this.toast_HowManyFavs();
+  }
+  //--Fin E  ---------------------------------------------------- 
 }
